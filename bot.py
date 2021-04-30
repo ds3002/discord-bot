c4ff8cf79e60b0443831de001bd23a912e0060b0
Neal Magee <nem2p@virginia.edu>
2021-04-30 12:34:22 -0400

    First commit

---
+++ bot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# import ssl
+import discord
+from discord.ext import commands
+import random
+import requests
+import os
+
+bot = commands.Bot(command_prefix = '.', verify=False)
+
+@bot.event
+async def on_ready():
+    print("This bot is ready to go!")
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} has joined')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def info(ctx):
+    await ctx.send('My available commands are:\n\n`.ping` - A simple ping\n`.8ball` - play a game')
+
+@bot.command()
+async def test(ctx, arg1, arg2):
+    await ctx.send('You passed {} and {}'.format(arg1, arg2))
+
+@bot.command(aliases=['8ball','eightball'])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.",
+                 "It is decidedly so.",
+                 "Without a doubt.",
+                 "Yes – definitely.",
+                 "You may rely on it.",
+                 "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+TOKEN = os.getenv('TOKEN')
+bot.run(TOKEN)
